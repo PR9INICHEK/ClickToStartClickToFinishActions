7a65ac37c852fa4a4ccf13d77eada1d4c677becc
PR9INICHEK <philliebe@gmail.com>
2021-11-03 08:24:27 +0300

    v3

---
+++ scripts/4_World/classes/useractionscomponent/actions/continuous/ActionLoadMagazine.c
@@ -0,0 +1,15 @@
+modded class ActionLoadMagazine
+{
+	override typename GetInputType()
+	{
+		return ClickContinuousDefaultActionInput;
+	}	
+}
+
+modded class ActionLoadMagazineQuick
+{
+	override typename GetInputType()
+	{
+		return ClickContinuousWeaponManipulationActionInput;
+	}	
+}
